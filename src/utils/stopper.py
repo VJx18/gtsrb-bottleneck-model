2c417ac9e823221352420c1957bc0c126ea62e76
tb-2000 <tomblum@gmx.de>
2025-12-12 11:12:51 +0100

    implement function to load best model state in stopper.py

--- src/utils/stopper.py
+++ src/utils/stopper.py
@@ -6,14 +6,20 @@ class EarlyStopper:
         self.min_delta = min_delta
         self.counter = 0
         self.min_val_loss = float("inf")
+        self.best_model_state = None
 
-    def early_stop(self, val_loss):
+    def early_stop(self, val_loss, model):
+ 
         if (val_loss < self.min_val_loss):
             self.min_val_loss = val_loss
+            self.best_model_state = model.state_dict()
             self.counter = 0
+            print(f"Validation loss improved from {self.min_val_loss:.6f} to {val_loss:.6f}. Saving best model...")
         elif (val_loss > self.min_val_loss + self.min_delta):
             self.counter += 1
             if (self.counter >= self.patience):
-                print(f"Early stop! \n")
                 return True
         return False
+    
+    def load_best_model(self, model):
+        model.load_state_dict(self.best_model_state)
